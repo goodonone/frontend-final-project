4889031cca09dc653818d2a6fab3ec9c826f9dee
code-jude <judeprashanth@outlook.com>
2023-09-01 12:16:54 -0700

    Setting up Controllers, models and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+  path: "",
+  redirectTo: "/home",
+  pathMatch: "full"
+},
+{
+  path: "signin",
+  component: SignInComponent
+},
+{
+  path: "signup",
+  component: SignUpComponent
+},
+{
+  path: "profile",
+  component: FrontpageComponent
+},
+{
+  path: "edit-profile",
+  component: EditProfileComponent
+},
+{
+  path: "edit-post",
+  component: EditPostComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { EditPostComponent } from './components/edit-post/edit-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent,
+    FrontpageComponent,
+    ProfileComponent,
+    EditProfileComponent,
+    EditPostComponent
   ],
   imports: [
     BrowserModule,
