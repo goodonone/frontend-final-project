2ebe4363572c0514bdfdce81debbd18acddee945
code-jude <judeprashanth@outlook.com>
2023-09-01 13:39:54 -0700

    Implemented Form Submit on HomePage

--- src/app/components/frontpage/frontpage.component.ts
+++ src/app/components/frontpage/frontpage.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostService } from 'src/app/services/post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-frontpage',
   templateUrl: './frontpage.component.html',
   styleUrls: ['./frontpage.component.css']
 })
-export class FrontpageComponent {
+export class FrontpageComponent implements OnInit {
+  postList: Post[] = [];
+  newPost: Post = new Post();
+
+  constructor(private postService: PostService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.postService.getAllPosts().subscribe(post => {
+      this.postList = post;
+    });
+  }
+
+  onSubmit(){
+    this.postService.createPost(this.newPost).subscribe(() => {
+      window.alert("Created Coffee Successfully");
+      // this.router.navigate(['coffee']);
+    }, error => {
+      console.log('Error: ', error)
+      if (error.status === 401 || error.status === 403) {
+        this.router.navigate(['signin']);
+      }
+    });
+  }
+
+
 
 }
